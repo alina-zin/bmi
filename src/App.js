75bd356cf86ba23ca3930af83da1dd66b6c56683
Alina Zinchenko <c0zial00@students.oamk.fi>
2021-01-25 19:08:02 +0200

    App ready

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+
+  const [height, setHeight] = useState(0);
+  const [weight, setWeight] = useState(0);
+  const [bmi, setBMI] = useState(0);
+
+  function countBMI(e) {
+    e.preventDefault();
+    const bmi = weight / (height * height);
+    setBMI(bmi);
+
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h2>BMI</h2>
+      <h3>Calculating body mass index</h3>
+      <form onSubmit={countBMI}>
+        <div>
+          <label>Height</label>
+          <input type="number" value={height} onChange={e => setHeight(e.target.value)}></input>
+        </div>
+        <div>
+          <label>Weight</label>
+          <input type="number" value={weight} onChange={e => setWeight(e.target.value)}></input>
+        </div>
+        <div>
+          <output>{bmi.toFixed(1)}</output>
+        </div>
+        <button>Calculate</button>
+      </form>
     </div>
   );
 }
